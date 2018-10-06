477363e762eaa3b5359a0b7e4690a7c32c250098
kos <kos@kos-6.local>
2018-10-06 09:53:22 +0900

    readme add

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # serialwaitagent
-You can use Arduino serial communication when multi serial communication.
+You can use Arduino serial communication when multi serial communication.  
+
+このプログラムはArduinoとRaspberryPiとのシリアル通信を複数のプログラムから実行の際の衝突回避プログラムです。  
+
+ユーザの必要に応じて自由にカスタムしたコードを生成することができます。  
+プログラム生成方法
+
+```
+ruby waitmaker.rb
+```
--- waitmaker.rb
+++ waitmaker.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 system("touch hinagata.py")
-puts "TranceWaitMaker. このプログラムはあなたのステータス設定ファイルを自動生成できます. happy coding!! by Iovesophy."
+puts "serialwaitagentMaker. このプログラムはあなたのステータス設定ファイルを自動生成できます. happy coding!! by Iovesophy."
 puts "数字は全て半角数字で入力してください。"
 puts "設定スタート:"
 puts "このプログラム自身のステータスナンバーを入力してください、ただし、他のプログラムステータスナンバーと同じナンバーは使わないでください。."
@@ -15,7 +15,7 @@ WAIT = gets.to_s.gsub(/(\r\n|\r|\n|\f)/,"")
 puts "設定したい処理待ちトライ回数を入力してください。"
 TRY = gets.to_s.gsub(/(\r\n|\r|\n|\f)/,"")
 
-#区分け処理、細かい判定を行う際しよう、（未実装）
+#区分け処理、細かい判定を行う際使用、（AgentMakerでは未実装）
 #puts "実行ステータスを区分けしますか?(特別な理由が無ければ、区分けは必要ありません)y,n:" 
 #SEP = gets.to_s.gsub(/(\r\n|\r|\n|\f)/,"")
 #if SEP == "y"
@@ -26,12 +26,12 @@ TRY = gets.to_s.gsub(/(\r\n|\r|\n|\f)/,"")
 File.open("hinagata.py","w") do |code|
 
 	code.puts "# -*- coding: utf-8 -*-"
-	code.puts "# transwaitprocessor made by Iovesophy --"
+	code.puts "# serialwaitagentprocessor made by Iovesophy --"
 	code.puts ""
 	code.puts "import serial"
 	code.puts "import time"
 	code.puts ""
-	code.puts "class TransWait():"
+	code.puts "class WaitAgent():"
 	code.puts ""
 	code.puts "    def my_status_set(self):"
 	code.puts "        w_file = open('#{PATH}',mode='w')"
